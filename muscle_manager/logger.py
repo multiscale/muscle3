44721020f1cbf5c42d3c19d2a3bd0788550aa3fc
Lourens Veen <l.veen@esciencecenter.nl>
2018-03-17 09:51:03 +0100

    Add missing docstrings to Logger

--- muscle_manager/logger.py
+++ muscle_manager/logger.py
@@ -5,6 +5,14 @@ from muscle_manager.operator import Operator
 
 
 class LogLevel(Enum):
+    """Log levels for MUSCLE 3.
+
+    These match the levels in the MUSCLE Manager Protocol, and should \
+    be kept identical to those. They also match the Python logging log \
+    levels, with an additional PROFILE level. This is to be used by \
+    libmuscle for messages intended for the built-in simple profiling \
+    algorithm.
+    """
     CRITICAL = 5
     ERROR = 4
     WARNING = 3
@@ -13,6 +21,7 @@ class LogLevel(Enum):
     DEBUG = 0
 
     def as_python_level(self) -> int:
+        """Convert the LogLevel to the corresponding Python level."""
         to_python_level = {
                 LogLevel.CRITICAL: logging.CRITICAL,
                 LogLevel.ERROR: logging.ERROR,
@@ -43,7 +52,17 @@ class Logger:
             time_stamp: str,
             level: LogLevel,
             text: str) -> None:
+        """Log a message.
 
+        Args:
+            instance_id: Identifier of the instance that generated the \
+                    message.
+            operator: The operator that generated the message.
+            time_stamp: Time when this log message was generated, \
+                    according to the clock on that machine.
+            level: The log level of the message.
+            text: The message text.
+        """
         self.__logger.log(
                 level.as_python_level(),
                 text,

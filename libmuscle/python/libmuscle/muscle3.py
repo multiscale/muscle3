1451c6425d6d6076baacd513ec7399c879d7ceb3
Lourens Veen <l.veen@esciencecenter.nl>
2019-01-10 20:22:16 +0100

    Add register method to Muscle3

--- libmuscle/python/libmuscle/muscle3.py
+++ libmuscle/python/libmuscle/muscle3.py
@@ -1,7 +1,10 @@
-from libmuscle.mmp_client import MMPClient
-
 import sys
-from typing import Optional
+from typing import Dict, List, Optional
+
+from ymmsl import Identifier, Operator, Port, Reference
+
+from libmuscle.compute_element import ComputeElement
+from libmuscle.mmp_client import MMPClient
 
 
 class Muscle3:
@@ -20,6 +23,62 @@ class Muscle3:
         mmp_location = self.__extract_manager_location()
         if mmp_location is not None:
             self.__manager = MMPClient(mmp_location)
+
+    def register(self, name: str, ports: Dict[Operator, List[str]],
+                 element: ComputeElement) -> None:
+        """Register a compute element with MUSCLE3.
+
+        Args:
+            name: The name of this element or instance.
+            ports: A dictionary mapping operators to a list of port
+                names for that operator.
+            element: The compute element to register.
+        """
+        full_name = self.__make_full_name(name)
+        locations = element._communicator.get_locations()
+        port_list = self.__port_list_from_ports(ports)
+        if self.__manager is not None:
+            self.__manager.register_instance(full_name, locations, port_list)
+            conduits, peer_dims, peer_locations = self.__manager.request_peers(
+                    full_name)
+            element._communicator.connect(conduits, peer_dims, peer_locations)
+
+    def __make_full_name(self, name: str) -> Reference:
+        """Makes a Reference of the name and optionally index.
+
+        If a --muscle-index=x,y,z is given on the command line, then
+        it is appended to the name.
+        """
+        full_name = Reference(name)
+
+        # Neither getopt, optparse, or argparse will let me pick out
+        # just one option from the command line and ignore the rest.
+        # So we do it by hand.
+        prefix = '--muscle-index='
+        for arg in sys.argv[1:]:
+            if arg.startswith(prefix):
+                index_str = arg[len(prefix):]
+                indices = index_str.split(',')
+                full_name += map(int, indices)
+                break
+
+        return full_name
+
+    def __port_list_from_ports(self, ports: Dict[Operator, List[str]]
+                               ) -> List[Port]:
+        """Converts a dict of ports per operator to a list of ports.
+
+        Args:
+            ports: The ports as passed by the user.
+
+        Returns:
+            The ports as expected by MMP.
+        """
+        result = list()
+        for operator, port_names in ports.items():
+            for name in port_names:
+                result.append(Port(Identifier(name), operator))
+        return result
 
     @staticmethod
     def __extract_manager_location() -> Optional[str]:
--- libmuscle/python/libmuscle/test/test_muscle3.py
+++ libmuscle/python/libmuscle/test/test_muscle3.py
@@ -1,20 +1,30 @@
 from libmuscle import Muscle3
 
-from unittest.mock import patch
 import pytest
+from ymmsl import Operator, Port, Reference
+
+from unittest.mock import MagicMock, patch
 import sys
 from typing import Generator
 
 
 @pytest.fixture
-def replaced_sys_argv() -> Generator[None, None, None]:
+def sys_argv_manager() -> Generator[None, None, None]:
     old_argv = sys.argv
     sys.argv = ['', '--muscle-manager=localhost:9000']
     yield None
     sys.argv = old_argv
 
 
-def test_extract_manager_location(replaced_sys_argv) -> None:
+@pytest.fixture
+def sys_argv_index() -> Generator[None, None, None]:
+    old_argv = sys.argv
+    sys.argv = ['', '--muscle-index=13,42']
+    yield
+    sys.argv = old_argv
+
+
+def test_extract_manager_location(sys_argv_manager) -> None:
     assert (Muscle3._Muscle3__extract_manager_location() ==
             'localhost:9000')
 
@@ -24,7 +34,67 @@ def test_muscle_init_no_manager() -> None:
     assert muscle._Muscle3__manager is None
 
 
-def test_muscle_init_manager(replaced_sys_argv) -> None:
+def test_muscle_init_manager(sys_argv_manager) -> None:
     with patch('libmuscle.muscle3.MMPClient') as mock_client:
         Muscle3()
         mock_client.assert_called_once_with('localhost:9000')
+
+
+def test_register() -> None:
+    muscle = Muscle3()
+    manager_client = MagicMock()
+    manager_client.request_peers = MagicMock(return_value=(1, 2, 3))
+    muscle._Muscle3__manager = manager_client
+
+    element = MagicMock()
+    ports = {
+            Operator.F_INIT: ['receive_init'],
+            Operator.O_F: ['send_output']}
+    muscle.register('test_model', ports, element)
+
+    args = manager_client.register_instance.call_args[0]
+    assert args[0] == Reference('test_model')
+    assert args[1] == element._communicator.get_locations.return_value
+    ports = sorted(args[2], key=lambda port: port.name)
+    assert ports[0].name == 'receive_init'
+    assert ports[0].operator == Operator.F_INIT
+    assert ports[1].name == 'send_output'
+    assert ports[1].operator == Operator.O_F
+
+    manager_client.request_peers.assert_called_with(
+            Reference('test_model'))
+
+    element._communicator.connect.assert_called_with(
+            *manager_client.request_peers.return_value)
+
+
+def test_register_instance(sys_argv_index) -> None:
+    muscle = Muscle3()
+    manager_client = MagicMock()
+    manager_client.request_peers = MagicMock(return_value=(1, 2, 3))
+    muscle._Muscle3__manager = manager_client
+
+    element = MagicMock()
+    ports = {
+            Operator.O_I: ['out_x', 'out_y'],
+            Operator.B: ['in_x', 'in_y']}
+    muscle.register('test_model', ports, element)
+
+    args = manager_client.register_instance.call_args[0]
+    assert args[0] == Reference('test_model[13][42]')
+    assert args[1] == element._communicator.get_locations.return_value
+    ports = sorted(args[2], key=lambda port: port.name)
+    assert ports[0].name == 'in_x'
+    assert ports[0].operator == Operator.B
+    assert ports[1].name == 'in_y'
+    assert ports[1].operator == Operator.B
+    assert ports[2].name == 'out_x'
+    assert ports[2].operator == Operator.O_I
+    assert ports[3].name == 'out_y'
+    assert ports[3].operator == Operator.O_I
+
+    manager_client.request_peers.assert_called_with(
+            Reference('test_model[13][42]'))
+
+    element._communicator.connect.assert_called_with(
+            *manager_client.request_peers.return_value)

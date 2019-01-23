e904d23c3ecf2d39341adff3b3404ab7155977b8
Lourens Veen <l.veen@esciencecenter.nl>
2019-01-23 22:31:01 +0100

    Add overlay configuration to ConfigurationStore

--- libmuscle/python/libmuscle/configuration_store.py
+++ libmuscle/python/libmuscle/configuration_store.py
@@ -12,16 +12,54 @@ class ConfigurationStore:
     def __init__(self) -> None:
         """Create a ConfigurationStore.
 
-        Initialises the base layer to an empty Configuration.
+        Initialises the base and overlay layers to an empty
+        Configuration.
+
+        A ConfigurationStore has two layers of configuration, a base
+        layer that contains an immutable set of parameters set in the
+        simulation's configuration, and an overlay layer that holds
+        parameter values that have been set at run-time.
         """
         self._base = Configuration()
+        self._overlay = Configuration()
 
     def set_base(self, base_config: Configuration) -> None:
+        """Set the base layer configuration.
+
+        Args:
+            base_config: The new base configuration
+        """
         self._base = base_config
+
+    def set_overlay(self, overlay_config: Configuration) -> None:
+        """Set the overlay configuration.
+
+        Args:
+            overlay_config: The new overlay configuration
+        """
+        self._overlay = overlay_config
 
     def get_parameter(self, name: Reference,
                       typ: Optional[str] = None) -> ParameterValue:
-        value = self._base[name]
+        """Returns the value of a parameter.
+
+        Args:
+            name: The name of the parameter to get the value of.
+            typ: An optional type designation; if specified the type
+                    is checked for a match before returning. Valid
+                    values are 'str', 'int', 'float', 'bool',
+                    '[float]' and '[[float]]'.
+
+        Raises:
+            KeyError: If the parameter has not been set.
+            TypeError: If the parameter was set to a value that does
+                    not match `typ`.
+            ValueError: If an invalid value was specified for `typ`
+        """
+        if name in self._overlay:
+            value = self._overlay[name]
+        else:
+            value = self._base[name]
         if typ is not None:
             if not has_parameter_type(value, typ):
                 raise TypeError('Value for parameter {} is of type {},'
--- libmuscle/python/libmuscle/test/test_configuration_store.py
+++ libmuscle/python/libmuscle/test/test_configuration_store.py
@@ -11,6 +11,7 @@ def store() -> ConfigurationStore:
 
 def test_create(store):
     assert len(store._base) == 0
+    assert len(store._overlay) == 0
 
 
 def test_set_base(store):
@@ -19,6 +20,21 @@ def test_set_base(store):
     assert store._base == config
 
 
+def test_set_overlay(store):
+    config = Configuration()
+    store.set_overlay(config)
+    assert store._overlay == config
+
+
 def test_get_parameter(store):
     store._base['test'] = 13
     assert store.get_parameter('test') == 13
+
+    store._overlay['test2'] = 14
+    assert store.get_parameter('test2') == 14
+
+    store._base['test2'] = 'test'
+    assert store.get_parameter('test2') == 14
+
+    store._overlay = Configuration()
+    assert store.get_parameter('test2') == 'test'

79568f6881cdbaac9dc7dc3baead0c52e0a93f16
Lourens Veen <l.veen@esciencecenter.nl>
2019-07-09 10:08:50 +0200

    Add most of ymmsl.identity

--- libmuscle/cpp/src/ymmsl/identity.cpp
+++ libmuscle/cpp/src/ymmsl/identity.cpp
@@ -1,13 +1,19 @@
+#include <algorithm>
+#include <iterator>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 
 #include "ymmsl/identity.hpp"
 
 
-namespace {
-    const char * invalid_identifier_msg = (
-            "Identifiers must consist only of lower- and uppercase letters,"
-            " digits and underscores, must start with a letter or an"
-            " underscore, and must not be empty.");
+using namespace std::string_literals;
+
+
+::std::size_t ::std::hash<::ymmsl::Identifier>::operator()(
+        argument_type const & id) const noexcept
+{
+    return hash<std::string>()(id.data_);
 }
 
 namespace ymmsl {
@@ -15,8 +21,13 @@ namespace ymmsl {
 Identifier::Identifier(std::string const & contents)
     : data_(contents)
 {
+    auto msg = "'" + contents + "' is not a valid identifier. Identifiers must"
+            " consist only of lower- and uppercase letters, digits and"
+            " underscores, must start with a letter or an underscore, and must"
+            " not be empty.";
+
     if (contents.empty())
-        throw std::invalid_argument(invalid_identifier_msg);
+        throw std::invalid_argument(msg);
 
     for (auto it = data_.cbegin(); it != data_.cend(); ++it) {
         bool is_lower = (('a' <= *it) && (*it <= 'z'));
@@ -26,18 +37,221 @@ Identifier::Identifier(std::string const & contents)
 
         bool valid_first_char = is_lower || is_upper || is_under;
         if (it == data_.cbegin() && !valid_first_char)
-            throw std::invalid_argument(invalid_identifier_msg);
+            throw std::invalid_argument(msg);
 
         if (!(is_lower || is_upper || is_digit || is_under))
-            throw std::invalid_argument(invalid_identifier_msg);
+            throw std::invalid_argument(msg);
     }
 }
 
+Identifier::operator std::string() const {
+    return data_;
+}
+
+bool Identifier::operator==(Identifier const & rhs) const {
+    return data_ == rhs.data_;
+}
+
+bool Identifier::operator==(std::string const & rhs) const {
+    return data_ == rhs;
+}
+
+bool Identifier::operator!=(Identifier const & rhs) const {
+    return data_ != rhs.data_;
+}
+
+bool Identifier::operator!=(std::string const & rhs) const {
+    return data_ != rhs;
+}
+
+bool operator==(std::string const & lhs, Identifier const & rhs) {
+    return lhs == rhs.data_;
+}
+
+bool operator!=(std::string const & lhs, Identifier const & rhs) {
+    return lhs != rhs.data_;
+}
 
 std::ostream & operator<<(std::ostream & os, Identifier const & i) {
     return os << i.data_;
 }
 
+ReferencePart::ReferencePart(Identifier const & i)
+    : identifier_(i)
+    , index_(-1)
+{}
+
+ReferencePart::ReferencePart(int index)
+    : identifier_("invalid")
+    , index_(index)
+{}
+
+bool ReferencePart::is_identifier() const {
+    return index_ < 0;
 }
 
+bool ReferencePart::is_index() const {
+    return index_ >= 0;
+}
+
+Identifier const & ReferencePart::identifier() const {
+    return identifier_;
+}
+
+int ReferencePart::index() const {
+    return index_;
+}
+
+bool ReferencePart::operator==(ReferencePart const & rhs) const {
+    if (is_identifier())
+        return rhs.is_identifier() && (identifier_ == rhs.identifier_);
+    return rhs.is_index() && (index_ == rhs.index_);
+}
+
+
+Reference::Reference(std::string const & content)
+    : parts_(string_to_parts_(content))
+{}
+
+Reference::operator std::string() const {
+    return Reference::parts_to_string_(parts_);
+}
+
+std::size_t Reference::length() const {
+    return parts_.size();
+}
+
+bool Reference::operator==(Reference const & rhs) const {
+    return std::equal(parts_.cbegin(), parts_.cend(), rhs.cbegin(), rhs.cend());
+}
+
+bool Reference::operator==(std::string const & rhs) const {
+    return parts_to_string_(parts_) == rhs;
+}
+
+bool Reference::operator!=(Reference const & rhs) const {
+    return !(*this == rhs);
+}
+
+bool Reference::operator!=(std::string const & rhs) const {
+    return !(*this == rhs);
+}
+
+Reference::const_iterator Reference::cbegin() const {
+    return parts_.cbegin();
+}
+
+Reference::const_iterator Reference::cend() const {
+    return parts_.cend();
+}
+
+Reference::const_iterator Reference::begin() const {
+    return parts_.cbegin();
+}
+
+Reference::const_iterator Reference::end() const {
+    return parts_.cend();
+}
+
+ReferencePart const & Reference::operator[](int i) const {
+    return parts_[i];
+}
+
+Reference Reference::operator+(Reference const & rhs) const {
+    std::vector<ReferencePart> new_parts(parts_);
+    std::copy(rhs.cbegin(), rhs.cend(), std::back_inserter(new_parts));
+    return Reference(std::move(new_parts));
+}
+
+Reference Reference::operator+(ReferencePart const & rhs) const {
+    std::vector<ReferencePart> new_parts(parts_);
+    new_parts.push_back(rhs);
+    return Reference(std::move(new_parts));
+}
+
+std::ostream & operator<<(std::ostream & os, Reference const & r) {
+    return os << Reference::parts_to_string_(r.parts_);
+}
+
+Reference::Reference(std::vector<ReferencePart> && parts)
+    : parts_(parts)
+{}
+
+namespace {
+    std::size_t find_next_op(std::string const & text, std::size_t start) {
+        auto next_bracket = text.find('[', start);
+        if (next_bracket == std::string::npos)
+            next_bracket = text.length();
+        auto next_period = text.find('.', start);
+        if (next_period == std::string::npos)
+            next_period = text.length();
+        return std::min(next_bracket, next_period);
+    }
+}
+
+std::vector<ReferencePart> Reference::string_to_parts_(std::string const & text) {
+    auto parts = std::vector<ReferencePart>();
+    auto end = text.length();
+
+    auto cur_op = find_next_op(text, 0u);
+    parts.push_back(Identifier(text.substr(0u, cur_op)));
+
+    while (cur_op < end) {
+        if (text[cur_op] == '.') {
+            auto next_op = find_next_op(text, cur_op + 1u);
+            auto id = Identifier(text.substr(cur_op + 1u, next_op - cur_op - 1u));
+            parts.push_back(ReferencePart(id));
+            cur_op = next_op;
+        }
+        else if (text[cur_op] == '[') {
+            auto close_bracket = text.find(']', cur_op);
+            if (close_bracket == std::string::npos)
+                throw std::invalid_argument("Missing closing bracket in"
+                                            " Reference " + text);
+            auto index_str = text.substr(cur_op + 1, close_bracket - cur_op - 1u);
+            try {
+                auto index = std::stoi(index_str);
+                parts.push_back(ReferencePart(index));
+            }
+            catch (std::invalid_argument const & e) {
+                throw std::invalid_argument("Invalid index '" + index_str +
+                        "' in " + text + ", expected an int.");
+            }
+            catch (std::out_of_range const & e) {
+                throw std::invalid_argument("Invalid index '" + index_str +
+                        "' in " + text + ", the value is out of range.");
+            }
+            cur_op = close_bracket + 1;
+        }
+        else {
+            throw std::invalid_argument("Invalid character '"s +
+                    text[cur_op] + " in Reference " + text);
+        }
+    }
+    return parts;
+}
+
+std::string Reference::parts_to_string_(std::vector<ReferencePart> const & parts) {
+    std::ostringstream oss;
+    for (auto it = parts.cbegin(); it != parts.cend(); ++it) {
+        if (it->is_identifier()) {
+            if (it != parts.cbegin())
+                oss << ".";
+            oss << it->identifier();
+        }
+        else
+            oss << "[" << it->index() << "]";
+    }
+    return oss.str();
+}
+
+bool operator==(::std::string const & lhs, Reference const & rhs) {
+    return rhs == lhs;
+}
+
+bool operator!=(::std::string const & lhs, Reference const & rhs) {
+    return rhs != lhs;
+}
+
+}
 
--- libmuscle/cpp/src/ymmsl/identity.hpp
+++ libmuscle/cpp/src/ymmsl/identity.hpp
@@ -1,20 +1,343 @@
 #pragma once
 
+#include <functional>
 #include <ostream>
 #include <string>
+#include <vector>
+
+
+namespace ymmsl {
+    class Identifier;
+}
+
+namespace std {
+    template<> struct hash<::ymmsl::Identifier> {
+        typedef ::ymmsl::Identifier argument_type;
+        typedef size_t result_type;
+
+        std::size_t operator()(::ymmsl::Identifier const &) const noexcept;
+    };
+}
 
 
 namespace ymmsl {
 
+/** A custom string type that represents an identifier.
+ *
+ * An identifier may consist of upper- and lowercase characters, digits, and
+ * underscores.
+ */
 class Identifier {
     public:
+        /** Create an Identifier.
+         *
+         * This creates a new identifier object, using the string
+         * representation of whichever object you pass.
+         *
+         * @param contents The contents of the identifier.
+         *
+         * @throws std::invalid_argument if contents is not a valid identifier.
+         */
         Identifier(std::string const & contents);
 
+        /** Implicit conversion to std::string.
+         *
+         * This is a custom string class, so that's appropriate.
+         *
+         * @return The string representation of this Identifier.
+         */
+        operator std::string() const;
+
+        /** Compare for equality.
+         *
+         * @param rhs The Identifier to compare against.
+         *
+         * @return True iff both Identifiers are equal.
+         */
+        bool operator==(Identifier const & rhs) const;
+
+        /** Compare for equality against a string.
+         *
+         * @param rhs The string to compare against.
+         *
+         * @return True iff the Identifier matches the string.
+         */
+        bool operator==(std::string const & rhs) const;
+
+        /** Compare for inequality.
+         *
+         * @param rhs The Identifier to compare against.
+         *
+         * @return True iff both Identifiers are different.
+         */
+        bool operator!=(Identifier const & rhs) const;
+
+        /** Compare for inequality against a string.
+         *
+         * @param rhs The string to compare against.
+         *
+         * @return True iff the Identifier is different from the string.
+         */
+        bool operator!=(std::string const & rhs) const;
+
+
     private:
+        friend bool operator==(std::string const & lhs, Identifier const & rhs);
+        friend bool operator!=(std::string const & lhs, Identifier const & rhs);
         friend std::ostream & operator<<(std::ostream & os, Identifier const & i);
+        friend ::std::size_t ::std::hash<::ymmsl::Identifier>::operator()(
+                ::ymmsl::Identifier const & id) const;
         std::string data_;
 };
 
 
+/** An item in a Reference.
+ */
+class ReferencePart {
+    public:
+        /** Create a ReferencePart containing an Identifier.
+         *
+         * This is implicit, so that you can give an Identifier wherever a
+         * ReferencePart is required.
+         *
+         * @param i The Identifier to store.
+         */
+        ReferencePart(Identifier const & i);
+
+        /** Create a ReferencePart containing an index.
+         *
+         * This is implicit, so that you can give an int wherever a
+         * ReferencePart is required.
+         *
+         * @param index The index to store.
+         */
+        ReferencePart(int index);
+
+        /** Returns whether this Part holds an Identifier.
+         *
+         * @return True iff this holds an Identifier.
+         */
+        bool is_identifier() const;
+
+        /** Returns whether this Part holds an index.
+         *
+         * @return True iff this holds an index.
+         */
+        bool is_index() const;
+
+        /** Returns the Identifier value.
+         *
+         * @return The identifier value.
+         *
+         * @throws std::runtime_error if this object does not hold an Identifier.
+         */
+        Identifier const & identifier() const;
+
+        /** Returns the index value.
+         *
+         * @return The index value.
+         *
+         * @throws std::runtime_error if this object does not hold an index.
+         */
+        int index() const;
+
+        /** Compares for equality.
+         *
+         * @param rhs The ReferencePart to compare with.
+         *
+         * @return True if both type and value match.
+         */
+        bool operator==(ReferencePart const & rhs) const;
+
+    private:
+        Identifier identifier_;
+        int index_;
+};
+
+
+/** A reference to an object in the MMSL execution model.
+ *
+ * References in string form are written as either:
+ *
+ * - an Identifier,
+ * - a Reference followed by a period and an Identifier, or
+ * - a Reference followed by an integer enclosed in square brackets.
+ *
+ * In object form, they consist of a list of Identifiers and ints. The first
+ * list item is always an Identifier. For the rest of the list, an Identifier
+ * represents a period operator with that argument, while an int represents the
+ * indexing operator with that argument.
+ *
+ * Reference objects act like a list of Identifiers and ints, you can get their
+ * length using length(), iterate through the parts using cbegin() and cend(),
+ * and get individual items using []. Note that the sublist has to be a valid
+ * Reference, so it cannot start with an int.
+
+ * References can be compared for equality to each other or to a
+ * plain string, and they can be used as dictionary keys. Reference
+ * objects are immutable (or they're supposed to be anyway), so do not
+ * try to change any of the elements. Instead, make a new Reference.
+ * Especially References that are used as dictionary keys must not be
+ * modified, this will get your dictionary in a very confused state.
+ */
+class Reference {
+    public:
+        /** Random access iterator type for a Reference.
+         */
+        typedef std::vector<ReferencePart>::const_iterator const_iterator;
+
+        /** Create a Reference from a string.
+         *
+         * Creates a Reference from a string, which will be parsed.
+         *
+         * @param contents A string to parse.
+         *
+         * @throws std::invalid_argument if the argument does not define a
+         *         valid Reference.
+         */
+        Reference(std::string const & content);
+
+        /** Conversion to std::string.
+         *
+         * @return The string representation of this Identifier.
+         */
+        explicit operator std::string() const;
+
+        /** Returns the number of parts in the Reference.
+         */
+        std::size_t length() const;
+
+        /** Compares for equality.
+         *
+         * Will compare part-by-part.
+         *
+         * @param rhs The Reference to compare with.
+         *
+         * @return True iff the two References are equal.
+         */
+        bool operator==(Reference const & rhs) const;
+
+        /** Compares for equality.
+         *
+         * Compares string representations.
+         *
+         * @param rhs The string to compare with.
+         *
+         * @return True iff this Reference matches the given string.
+         */
+        bool operator==(std::string const & rhs) const;
+
+        /** Compares for inequality.
+         *
+         * Will compare part-by-part.
+         *
+         * @param rhs The Reference to compare with.
+         *
+         * @return True iff the two References are different.
+         */
+        bool operator!=(Reference const & rhs) const;
+
+        /** Compares for inequality.
+         *
+         * Compares string representations.
+         *
+         * @param rhs The string to compare with.
+         *
+         * @return True iff this Reference does not match the given string.
+         */
+        bool operator!=(std::string const & rhs) const;
+
+        /** Returns a const_iterator to the beginning of the Reference.
+         */
+        const_iterator cbegin() const;
+
+        /** Returns a const_iterator to one-past-the-end of the Reference.
+         */
+        const_iterator cend() const;
+
+        /** Returns a const_iterator to the beginning of the Reference.
+         */
+        const_iterator begin() const;
+
+        /** Returns a const_iterator to one-past-the-end of the Reference.
+         */
+        const_iterator end() const;
+
+        /** Returns the ReferencePart at the given index.
+         *
+         * @param i The index to dereference.
+         * @return The ReferencePart at that index.
+         */
+        ReferencePart const & operator[](int i) const;
+
+        /** Concatenate two References.
+         *
+         * @param rhs The Reference to append to this one to produce the new
+         *        Reference.
+         */
+        Reference operator+(Reference const & rhs) const;
+
+        /** Append a ReferencePart to this Reference to create a new one.
+         *
+         * @param rhs The ReferencePart to append to this one to produce the
+         *        new Reference.
+         */
+        Reference operator+(ReferencePart const & rhs) const;
+
+    private:
+        friend std::ostream & operator<<(std::ostream & os, Reference const & r);
+
+        std::vector<ReferencePart> parts_;
+
+        Reference(std::vector<ReferencePart> && parts);
+        static std::vector<ReferencePart> string_to_parts_(
+                std::string const & text);
+        static std::string parts_to_string_(
+                std::vector<ReferencePart> const & parts);
+};
+
+/** Compare a string with a Reference for equality.
+ *
+ * @param lhs The left-hand side of the comparison.
+ * @param rhs The right-hand side of the comparison.
+ *
+ * @return True iff the string matches the Reference.
+ */
+bool operator==(std::string const & lhs, Reference const & rhs);
+
+/** Compare a string with a Reference for equality.
+ *
+ * @param lhs The left-hand side of the comparison.
+ * @param rhs The right-hand side of the comparison.
+ *
+ * @return True iff the string does not match the Reference.
+ */
+bool operator!=(std::string const & lhs, Reference const & rhs);
+
+}
+
+namespace std {
+    template<> struct hash<::ymmsl::ReferencePart> {
+        typedef ::ymmsl::ReferencePart argument_type;
+        typedef size_t result_type;
+
+        result_type operator()(argument_type const & refpart) const noexcept {
+            if (refpart.is_identifier())
+                return hash<::ymmsl::Identifier>()(refpart.identifier());
+            return hash<int>()(refpart.index());
+        }
+    };
+
+    template<> struct hash<::ymmsl::Reference> {
+        typedef ::ymmsl::Reference argument_type;
+        typedef size_t result_type;
+
+        result_type operator()(argument_type const & ref) const noexcept {
+            result_type res = 0ul;
+            for (auto const & part : ref)
+                res ^= hash<::ymmsl::ReferencePart>()(part) + 0x9e3779b9 + (res << 6) + (res >> 2);
+            return res;
+        }
+    };
 }
 

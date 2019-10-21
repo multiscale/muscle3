69e62865f150c9dcf2d1b00afe93757f3af41b95
Lourens Veen <l.veen@esciencecenter.nl>
2019-10-21 13:18:54 +0200

    Add a Python/C++ integration test

--- integration_test/conftest.py
+++ integration_test/conftest.py
@@ -21,19 +21,38 @@ def yatiml_log_warning():
     yatiml.logger.setLevel(logging.WARNING)
 
 
-def start_mmp_server(control_pipe, ymmsl):
-    control_pipe[0].close()
-
+def make_server(ymmsl_doc: ymmsl.Configuration):
     logger = Logger()
-    expected_elements = elements_for_model(ymmsl.model)
+    expected_elements = elements_for_model(ymmsl_doc.model)
     instance_registry = InstanceRegistry(expected_elements)
-    topology_store = TopologyStore(ymmsl)
-    server = MMPServer(logger, ymmsl.settings, instance_registry,
+    topology_store = TopologyStore(ymmsl_doc)
+    server = MMPServer(logger, ymmsl_doc.settings, instance_registry,
                        topology_store)
+    return server
+
+
+def start_mmp_server(control_pipe, ymmsl_doc):
+    control_pipe[0].close()
+    server = make_server(ymmsl_doc)
     control_pipe[1].send(True)
     control_pipe[1].recv()
     control_pipe[1].close()
     server.stop()
+
+
+def make_server_process(ymmsl_doc):
+    control_pipe = mp.Pipe()
+    process = mp.Process(target=start_mmp_server,
+                         args=(control_pipe, ymmsl_doc),
+                         name='MMPServer')
+    process.start()
+    control_pipe[1].close()
+    # wait for start
+    control_pipe[0].recv()
+    yield None
+    control_pipe[0].send(True)
+    control_pipe[0].close()
+    process.join()
 
 
 @pytest.fixture
@@ -62,18 +81,34 @@ def mmp_server_process(yatiml_log_warning):
             )
     ymmsl_doc = ymmsl.load(ymmsl_text)
 
-    control_pipe = mp.Pipe()
-    process = mp.Process(target=start_mmp_server,
-                         args=(control_pipe, ymmsl_doc),
-                         name='MMPServer')
-    process.start()
-    control_pipe[1].close()
-    # wait for start
-    control_pipe[0].recv()
-    yield None
-    control_pipe[0].send(True)
-    control_pipe[0].close()
-    process.join()
+    yield from make_server_process(ymmsl_doc)
+
+
+@pytest.fixture
+def mmp_server_process_simple(yatiml_log_warning):
+    ymmsl_text = (
+            'ymmsl_version: v0.1\n'
+            'model:\n'
+            '  name: test_model\n'
+            '  compute_elements:\n'
+            '    macro: macro_implementation\n'
+            '    micro: micro_implementation\n'
+            '  conduits:\n'
+            '    macro.out: micro.in\n'
+            '    micro.out: macro.in\n'
+            'settings:\n'
+            '  test1: 13\n'
+            '  test2: 13.3\n'
+            '  test3: testing\n'
+            '  test4: True\n'
+            '  test5: [2.3, 5.6]\n'
+            '  test6:\n'
+            '    - [1.0, 2.0]\n'
+            '    - [3.0, 1.0]\n'
+            )
+    ymmsl_doc = ymmsl.load(ymmsl_text)
+
+    yield from make_server_process(ymmsl_doc)
 
 
 @pytest.fixture
@@ -100,14 +135,9 @@ def mmp_server(yatiml_log_warning):
             '    - [1.0, 2.0]\n'
             '    - [3.0, 1.0]\n'
             )
-
-    logger = Logger()
     ymmsl_doc = ymmsl.load(ymmsl_text)
-    expected_elements = elements_for_model(ymmsl_doc.model)
-    instance_registry = InstanceRegistry(expected_elements)
-    topology_store = TopologyStore(ymmsl_doc)
-    server = MMPServer(logger, ymmsl_doc.settings, instance_registry,
-                       topology_store)
+
+    server = make_server(ymmsl_doc)
     yield server
     server.stop()
 

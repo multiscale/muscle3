4e4bb97b2b547b53e6500dd501dd75f9a26da015
Lourens Veen <l.veen@esciencecenter.nl>
2020-04-16 21:29:09 +0200

    Try pre-installing a lower version of Jinja and MarkupSafe instead

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         'pytest-runner',
         # dependencies for `python setup.py build_sphinx`
         'breathe',
-        'MarkupSafe<2',
         'sphinx',
         'recommonmark',
         'sphinx-rtd-theme'

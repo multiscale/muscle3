29e7488d73f4134849fc115d70613b10ee2315ac
Lourens Veen <l.veen@esciencecenter.nl>
2022-05-22 22:07:23 +0200

    Tidy up request types a bit

--- libmuscle/cpp/src/libmuscle/mcp/protocol.hpp
+++ libmuscle/cpp/src/libmuscle/mcp/protocol.hpp
@@ -5,23 +5,26 @@ namespace libmuscle { namespace impl {
 /** Request type for MMP requests
  *
  * Needs to be kept in sync with the corresponding definition on the Python
- * side, including the numerical values.
+ * side, including the numerical values. See there for an explanation.
  */
 enum class RequestType {
+    // MUSCLE Manager Protocol
     register_instance = 1,
     get_peers = 2,
     deregister_instance = 3,
     get_settings = 4,
-    get_next_message = 6,
-    submit_log_message = 7,
-    submit_profile_events = 8
+    submit_log_message = 5,
+    submit_profile_events = 6,
+
+    // MUSCLE Peer Protocol
+    get_next_message = 21
 };
 
 
 /** Response type for MMP responses
  *
  * Needs to be kept in sync with the corresponding definition on the Python
- * side, including the numerical values.
+ * side, including the numerical values. See there for an explanation.
  */
 enum class ResponseType {
     success = 0,

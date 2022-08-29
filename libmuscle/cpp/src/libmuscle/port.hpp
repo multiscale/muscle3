5764f9e4155c9831f08e54420fa7e77c132fc234
Maarten Sebregts <msebregts@ignitioncomputing.com>
2022-08-29 09:49:40 +0200

    Implement message counters on Port (C++)

--- libmuscle/cpp/src/libmuscle/port.hpp
+++ libmuscle/cpp/src/libmuscle/port.hpp
@@ -11,6 +11,10 @@ namespace libmuscle { namespace impl {
  * Ports can be used to send or receive messages. They have a name and an
  * operator, as well as a set of dimensions that determines the valid slot
  * indices for sending or receiving on this port.
+ *
+ * Ports keep track of the amount of messages sent or received on the port.
+ * However, the actual incrementing and validation is done in
+ * Communicator.
  */
 class Port : public ::ymmsl::Port {
     public:
@@ -26,7 +30,8 @@ class Port : public ::ymmsl::Port {
         Port(
                 std::string const & name, ::ymmsl::Operator oper,
                 bool is_vector, bool is_connected,
-                int our_ndims, std::vector<int> peer_dims);
+                int our_ndims, std::vector<int> peer_dims,
+                std::vector<int> num_messages=std::vector<int>(0));
 
         // Note: we only ever use this Port in libmuscle, and only use
         // ymmsl::Port in ymmsl. Port objects are always handled by value, so
@@ -104,11 +109,67 @@ class Port : public ::ymmsl::Port {
          */
         void set_closed(int slot);
 
+        /** Increment amount of messages sent or received.
+         */
+        void increment_num_messages();
+
+        /** Increment amount of messages sent or received.
+         *
+         * Only valid for vector ports.
+         *
+         * @param slot The slot that is sent/received on
+         */
+        void increment_num_messages(int slot);
+
+        /** Get the amount of messages sent or received
+         */
+        int get_num_messages() const;
+
+        /** Get the amount of messages sent or received
+         *
+         * Only valid for vector ports.
+         *
+         * @param slot The slot that is sent/received on
+         */
+        int get_num_messages(int slot) const;
+
+        /** True when this port has resumed.
+         *
+         * After resumption, each port/slot may discard exactly one message.
+         * is_resuming keeps track of this state.
+         */
+        bool is_resuming() const;
+
+        /** True when this port has resumed.
+         *
+         * After resumption, each port/slot may discard exactly one message.
+         * is_resuming keeps track of this state.
+         *
+         * Only valid for vector ports.
+         *
+         * @param slot The slot that is sent/received on
+         */
+        bool is_resuming(int slot) const;
+
+        /** Mark that this port has resumed and may no longer discard messages.
+         */
+        void set_resumed();
+
+        /** Mark that this port has resumed and may no longer discard messages.
+         *
+         * Only valid for vector ports.
+         *
+         * @param slot The slot that is sent/received on
+         */
+        void set_resumed(int slot);
+
     private:
         bool is_connected_;
         int length_;
         bool is_resizable_;
         std::vector<bool> is_open_;
+        std::vector<int> num_messages_;
+        std::vector<bool> is_resuming_;
 };
 
 } }

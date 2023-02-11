fd25c8b8efee058510413cf40bfbc80946358dda
Lourens Veen <l.veen@esciencecenter.nl>
2023-02-11 23:04:14 +0100

    Use cleaner syntax for converting time

--- libmuscle/cpp/src/libmuscle/timestamp.cpp
+++ libmuscle/cpp/src/libmuscle/timestamp.cpp
@@ -12,8 +12,7 @@ namespace libmuscle { namespace impl {
 
 Timestamp::Timestamp() {
     auto since_epoch = wallclock::now().time_since_epoch();
-    double cycles = since_epoch.count();
-    seconds = cycles * wallclock::period::num / wallclock::period::den;
+    seconds = std::chrono::duration<double>(since_epoch).count();
 }
 
 Timestamp::Timestamp(double seconds)

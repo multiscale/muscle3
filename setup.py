1bb85f3c9a5c4d576ed8c2c790284118b98a87e3
Lourens Veen <l.veen@esciencecenter.nl>
2023-09-27 21:58:26 +0200

    Add Python 3.11 support to package metadata

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'],
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'],
 
     packages=_muscle3_packages,
     package_dir={

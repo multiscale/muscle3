2c72c9e61ab4701523aec867c3e1e602e666c51a
Lourens Veen <l.veen@esciencecenter.nl>
2024-09-20 20:52:18 +0200

    Fix dispatch clustert test case

--- integration_test/cluster_test/dispatch.sh
+++ integration_test/cluster_test/dispatch.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#SBATCH --time=0:1:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-
 set -e
 
-source /home/cerulean/venv/bin/activate
+source /home/cerulean/shared/venv/bin/activate
 
-muscle_manager --log-level=DEBUG --start-all /home/cerulean/cluster_test/dispatch.ymmsl /home/cerulean/cluster_test/settings.ymmsl /home/cerulean/cluster_test/implementations.ymmsl
+CT=/home/cerulean/shared/cluster_test
+
+muscle_manager --log-level=DEBUG --start-all $CT/dispatch.ymmsl $CT/settings.ymmsl $CT/implementations.ymmsl
 

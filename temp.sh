56369085d5b8ce2ce716a240dcf689e49c10e6ee
Pranav Shridhar <pranavshridhar0@gmail.com>
2025-08-16 11:50:16 -0500

    Removed temp.sh as it is no longer needed

--- temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Because I hate writing this over and over again
-# Note to the reviewer: delete this script per your discretion
-
-echo "=== Building C++ tests ==="
-make cpp_tests \
-    > >(tee build_cpp_tests_out.log) \
-    2> >(tee build_cpp_tests_err.log >&2)

750498934c306d362887f36256e51febb683cbd8
Iris van der Werf <ivanderwerf@ignitioncomputing.com>
2026-03-02 17:56:51 +0100

    fix length of lines

--- scripts/make_libmuscle_api.py
+++ scripts/make_libmuscle_api.py
@@ -969,14 +969,19 @@ instance_members = [
          Vec2Dbl('value')],
         T(), 'get_setting_as', [String('name')], True),
     MemFunTmpl(
-        [String(), Int64t(), Double(), Bool(), VecInt64t('value'), VecDbl('value'),
-         Vec2Dbl('value')],
-        T(), 'get_setting_with_default_as', [String('name'), T('default_value')], True,
-        cpp_chain_call=lambda cpp_args, tpl_type, **kwargs: f'self_p->get_setting_as<{tpl_type}>({cpp_args})'),
+        [String(), Int64t(), Double(), Bool(), VecInt64t('value'),
+         VecDbl('value'), Vec2Dbl('value')],
+        T(), 'get_setting_with_default_as',
+        [String('name'), T('default_value')], True,
+        cpp_chain_call=lambda cpp_args, tpl_type, **kwargs: (
+            f'self_p->get_setting_as<{tpl_type}>({cpp_args})')),
     OverloadSet('get_setting_with_default', [
-        'get_setting_with_default_as_character', 'get_setting_with_default_as_int8',
-        'get_setting_with_default_as_real8', 'get_setting_with_default_as_logical',
-        'get_setting_with_default_as_int8array', 'get_setting_with_default_as_real8array',
+        'get_setting_with_default_as_character',
+        'get_setting_with_default_as_int8',
+        'get_setting_with_default_as_real8',
+        'get_setting_with_default_as_logical',
+        'get_setting_with_default_as_int8array',
+        'get_setting_with_default_as_real8array',
         'get_setting_with_default_as_real8array2'
         ], False),
     MemFun(VecString('value'), 'list_settings'),

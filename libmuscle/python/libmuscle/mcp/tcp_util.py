dbb141b56747175a4ce2dc34f9f09ecca079f48e
Iris van der Werf <ivanderwerf@ignitioncomputing.com>
2026-05-11 16:19:46 +0200

    remove bytes changes

--- libmuscle/python/libmuscle/mcp/tcp_util.py
+++ libmuscle/python/libmuscle/mcp/tcp_util.py
@@ -53,7 +53,7 @@ def recv_all(socket: SocketType, length: int) -> bytes:
 
         received_count += received_now
 
-    return bytes(databuf)
+    return databuf
 
 
 def send_int64(socket: SocketType, data: int) -> None:
